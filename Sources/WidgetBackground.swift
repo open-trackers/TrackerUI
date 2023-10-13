4913e5200ef43c018cff51712177a50848fb9ceb
Reed Es <reedes@fastmail.com>
2023-10-13 13:36:24 -0600

    fix for container background warning

--- Sources/WidgetBackground.swift
+++ Sources/WidgetBackground.swift
@@ -1,18 +1,32 @@
 //
-//  SwiftUIView.swift
-//  
+//  WidgetBackground.swift
 //
-//  Created by Reed Esau on 10/13/23.
+// Copyright 2023  OpenAlloc LLC
 //
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
 
 import SwiftUI
+import WidgetKit
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+// Usage:
+//
+// In widget view, add:
+//
+// .widgetBackground(backgroundView: Color.clear)
+
+extension View {
+    public func widgetBackground(backgroundView: some View) -> some View {
+        if #available(watchOS 10.0, iOSApplicationExtension 17.0, iOS 17.0, macOSApplicationExtension 14.0, *) {
+            return containerBackground(for: .widget) {
+                backgroundView
+            }
+        } else {
+            return background(backgroundView)
+        }
     }
 }
 
-#Preview {
-    SwiftUIView()
-}
